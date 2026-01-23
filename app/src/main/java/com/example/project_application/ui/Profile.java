048fefc9bd958a3238b6bf7ff487e2ee8218c616
yanseeeeeeeeee <anasemenova56966@gmail.com>
2026-01-23 14:53:33 +0700

    нижнее меню и экран профиля

---
+++ app/src/main/java/com/example/project_application/ui/Profile.java
@@ -0,0 +1,17 @@
+package com.example.project_application.ui;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.project_application.R;
+
+public class Profile extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
